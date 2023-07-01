4067c3236d14b8c15acbd37735f3fb07133a85b5
Alain Iglesias <alain.iherrera@gmail.com>
2023-06-30 18:07:34 -0600

    refactor: update banner - integrate current user

--- src/layout/banner.tsx
+++ src/layout/banner.tsx
@@ -1,15 +1,18 @@
+'use client'
+
+import { useUser } from '@clerk/nextjs'
 import { XMarkIcon } from '@heroicons/react/20/solid'
 
-const Banner = ({
-  name = 'Alain',
-  message = 'Esta será tu herramienta de apoyo en tu proceso de Onboarding',
-  canClose = false,
-}) => {
+const Banner = ({ message = 'Esta será tu herramienta de apoyo en tu proceso de Onboarding', canClose = false }) => {
+  const { isLoaded, isSignedIn, user } = useUser()
+  const year = new Date().getFullYear()
+
   return (
     <div className="flex items-center gap-x-6 bg-gray-50 bg-gradient-to-r from-[#BED731] to-[#7CB13D] px-6 py-2 sm:px-3.5 sm:before:flex-1">
       <div className="flex h-auto flex-wrap items-center gap-x-4 gap-y-2">
         <p className="text-base leading-6 text-slate-100">
-          <strong className="font-semibold">APP 2023</strong> • Hola {name}, bienvenido! | {message}.
+          <strong className="font-semibold">APP {year}</strong> • Hola {isLoaded && isSignedIn && user.firstName},
+          bienvenido! | {message}.
         </p>
       </div>
 
